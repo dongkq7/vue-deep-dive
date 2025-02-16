69295000bc2f6550db76cefca589096c3f14ba72
dongkaiqi <87631658@qq.com>
2025-02-16 15:41:41 +0800

    transition group

--- 06-一些细节补充/vue-project/src/App.vue
+++ 06-一些细节补充/vue-project/src/App.vue
@@ -1,104 +1,97 @@
 <template>
   <div class="container">
-    <div class="btns">
-      <button @click="prev">上一张</button>
-      <button @click="next">下一张</button>
-    </div>
-    <!-- 根据不同的方向，name不同 -->
-    <!-- 下一张：next-image -->
-    <!-- 上一张：prev-image -->
-    <Transition :name="`${direction}-image`">
-      <img class="image" :key="curIndex" :src="curImage" />
-    </Transition>
+    <input
+      type="text"
+      v-model="newContent"
+      class="todo-content"
+      placeholder="请输入新的待办事项"
+      @keypress.enter="addNewItem"
+    />
   </div>
+  <TransitionGroup tag="ul" name="fade" class="todo-container">
+    <li v-for="item in todos" :key="item.id" class="todo">
+      <span>{{ item.content }}</span>
+      <button @click="deleteItem(item)">删除</button>
+    </li>
+  </TransitionGroup>
 </template>
 
 <script setup>
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
 
-// 定义一个图片索引
-const curIndex = ref(0)
+const newContent = ref('')
 
-// 图片数组
-const images = [
-  'https://10.idqqimg.com/eth/ajNVdqHZLLAJib8odhz8Th2Z4Gat0axooYaxANJlaLEwTomre0hx8Y5yib6FxDZxsgiaYG1W2ETbrU/130?tp=webp',
-  'https://10.idqqimg.com/eth/ajNVdqHZLLDqYf0PtFibF9JNOnRbAw7DicWPicmfRkQwPeK2mnZ7ZJzZFdsCwCWdcwhEqoVphXiaDHE/130?tp=webp',
-  'https://thirdqq.qlogo.cn/g?b=sdk&k=LaERpMuX1ZjWTQmhrhst6Q&s=100&t=0&tp=webp',
-  'https://10.idqqimg.com/eth/ajNVdqHZLLDXIjdTYsqbfkxiaibd3lYGEgfiaEwficYfK2ogZDicCxaKibVibGA2Cj2ltgOvCm1tbRs1iac/130?tp=webp',
-  'https://thirdqq.qlogo.cn/g?b=sdk&k=pfIficic6WRliaLULZudVI5Tw&s=640&t=1600139160&tp=webp'
-]
-
-// 定义一个移动方向
-const direction = ref('next')
-
-// 根据当前索引返回对应图片
-const curImage = computed(() => images[curIndex.value])
-// 最大索引值
-const maxIndex = computed(() => images.length - 1)
-
-function prev() {
-  curIndex.value--
-  if (curIndex.value < 0) {
-    // 跳转到最后一张
-    curIndex.value = maxIndex.value
-  }
-  direction.value = 'prev'
+/**
+ * 生成随机id
+ */
+function randomId() {
+  return Math.random().toString(36).substr(2, 9)
 }
 
-function next() {
-  curIndex.value++
-  if (curIndex.value > maxIndex.value) {
-    // 跳转到第一张
-    curIndex.value = 0
-  }
-  direction.value = 'next'
+const todos = ref([
+  { id: randomId(), content: '任务1' },
+  { id: randomId(), content: '任务2' },
+  { id: randomId(), content: '任务3' }
+])
+
+function deleteItem(item) {
+  todos.value = todos.value.filter((todo) => todo.id !== item.id)
+}
+
+function addNewItem() {
+  if (newContent.value.trim() === '') return
+  todos.value.unshift({
+    id: randomId(),
+    content: newContent.value
+  })
+  newContent.value = ''
 }
 </script>
 
 <style scoped>
-/* 容器样式 */
 .container {
-  text-align: center;
+  width: 600px;
+  margin: 1em auto;
+  padding: 1.5em;
+  border-radius: 5px;
+}
+.shuffle {
+  margin: 1em 0;
+}
+.todo-content {
+  box-sizing: border-box;
+  width: 100%;
+  height: 50px;
+  border-radius: 5px;
+  outline: none;
+  font-size: 1.3em;
+  padding: 0 1em;
+  border: 1px solid #ccc;
+}
+.todo-container {
+  list-style: none;
+  padding: 0;
+  margin: 1em 0;
+}
+.todo {
+  padding: 0.5em 0;
+  border-bottom: 1px solid #ccc;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 1em;
 }
 
-/* 按钮样式 */
-.btns button {
-  margin: 1em 0.5em;
-}
+/* xxx-enter-active 新元素进入的时候会挂这个类 */
+/* xxx-leave-active 元素离开的时候会挂这个类 */
+/* xxx-move 其他元素涉及到移动的时候，会挂这个类 */
 
-/* 图片样式 */
-.image {
-  width: 200px;
-  height: 200px;
-  border-radius: 50%;
-  position: absolute;
-  left: 50%;
-  margin-left: -100px;
-  top: 100px;
-}
-
-/* active阶段需要过渡 */
-.next-image-enter-active,
-.next-image-leave-active,
-.prev-image-enter-active,
-.prev-image-leave-active {
+.fade-enter-active,
+.fade-leave-active,
+.fade-move {
   transition: 0.5s;
 }
-
-.next-image-enter-from,
-.next-image-leave-to,
-.prev-image-enter-from,
-.prev-image-leave-to {
+.fade-leave-to {
   opacity: 0;
-}
-
-.next-image-enter-from,
-.prev-image-leave-to {
-  transform: translateX(200px);
-}
-
-.next-image-leave-to,
-.prev-image-enter-from {
-  transform: translateX(-200px);
+  transform: translateX(100%);
 }
 </style>
